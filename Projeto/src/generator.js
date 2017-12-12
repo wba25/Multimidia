92d75305992c86e27e09e4399a5f20444efbfb33
Wellington Almeida <wba@cin.ufpe.br>
2017-12-11 23:22:51 -0300

    Start index page

---
+++ Projeto/src/generator.js
@@ -0,0 +1,16 @@
+function initApp() {
+    $("#listagem").hide();
+    $("#showPlaylist").hide();
+    $("#createPlaylist").hide();
+
+    $("#getPlays").on('click', function() {
+        //
+    });
+}
+
+$(document).ready(
+    function() {
+        initApp();
+        //performAuthDance();
+    }
+);
